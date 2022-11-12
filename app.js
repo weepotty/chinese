bee68fe8814720882b3955d0abee4f7a51a49a08
weepotty <99415923+weepotty@users.noreply.github.com>
2022-11-12 18:12:52 +0800

    add rest of audio files

---
+++ app.js
@@ -0,0 +1,82 @@
+
+    
+    let audioOpen = document.getElementById('audioOpen')
+    let playOpen = function () {
+audioOpen.play()
+    }
+
+
+    let audioClose = document.getElementById('audioClose')
+    let playClose = function () {
+audioClose.play()
+    }
+
+    let audioBlink = document.getElementById('audioBlink')
+    let playBlink = function () {
+audioBlink.play()
+    }
+
+
+let audioUp = document.getElementById('audioUp')
+    let playUp = function () {
+audioUp.play()
+    }
+
+let audioDown = document.getElementById('audioDown')
+    let playDown = function () {
+audioDown.play()
+    }
+
+let audioLeft = document.getElementById('audioLeft')
+    let playLeft = function () {
+audioLeft.play()
+    }
+
+let audioRight = document.getElementById('audioRight')
+    let playRight = function () {
+audioRight.play()
+    }
+
+let audioEar = document.getElementById('audioEar')
+    let playEar = function () {
+audioEar.play()
+    }
+
+let audioLight = document.getElementById('audioLight')
+    let playLight = function () {
+audioLight.play()
+    }
+
+    let audioStraight = document.getElementById('audioStraight')
+    let playStraight = function () {
+audioStraight.play()
+    }
+
+let audioSqueeze = document.getElementById('audioSqueeze')
+    let playSqueeze = function () {
+audioSqueeze.play()
+    }
+
+let audioDrop = document.getElementById('audioDrop')
+    let playDrop = function () {
+audioDrop.play()
+    }
+
+
+let audioChin = document.getElementById('audioChin')
+    let playChin = function () {
+audioChin.play()
+    }
+
+
+let audioSit = document.getElementById('audioSit')
+    let playSit = function () {
+audioSit.play()
+    }
+
+
+
+
+
+
+
